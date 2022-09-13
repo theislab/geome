05bc4c5386a78b4952ebbb2ba04b6aeaff08ef79
syelman <syozleyen@gmail.com>
2022-09-13 16:54:49 +0200

    transform list to a batch if it's a list

--- src/gpu_spatial_graph_pipeline/models/linear_ncem.py
+++ src/gpu_spatial_graph_pipeline/models/linear_ncem.py
@@ -8,7 +8,7 @@ import torch.optim as optim
 from sklearn.metrics import r2_score
 from ..models.modules.linear_model import LinearNonspatial, LinearSpatial
 import numpy as np
-
+from torch_geometric.data import Batch
 
 class LinearNCEM(pl.LightningModule):
     def __init__(self, model_type="spatial", use_node_scale=False, **model_kwargs):
@@ -104,7 +104,7 @@ class LinearNCEM(pl.LightningModule):
 
     def training_step(self, batch, _):
         if type(batch) == list:
-            batch = batch[0]
+            batch = Batch.from_data_list(batch)
         mu, sigma = self.forward(batch)
         loss = self.loss_module(mu[:self.batch_size], batch.y[:self.batch_size], sigma[:self.batch_size])
         self.log("train_loss", loss)
@@ -112,7 +112,7 @@ class LinearNCEM(pl.LightningModule):
 
     def validation_step(self, batch, _):
         if type(batch) == list:
-            batch = batch[0]
+            batch = Batch.from_data_list(batch)
         mu, sigma = self.forward(batch)
         val_loss = self.loss_module(mu[:self.batch_size], batch.y[:self.batch_size], sigma[:self.batch_size])
         val_r2_score = r2_score(batch.y.cpu()[:self.batch_size], mu.cpu()[:self.batch_size])
@@ -121,7 +121,7 @@ class LinearNCEM(pl.LightningModule):
 
     def test_step(self, batch, _):
         if type(batch) == list:
-            batch = batch[0]
+            batch = Batch.from_data_list(batch)
         mu, sigma = self.forward(batch)
         test_loss = self.loss_module(mu[:self.batch_size], batch.y[:self.batch_size], sigma[:self.batch_size])
         test_r2_score = r2_score(batch.y.cpu()[:self.batch_size], mu.cpu()[:self.batch_size])
