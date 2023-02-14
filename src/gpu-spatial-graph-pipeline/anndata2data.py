8e0062ec81dd7377fb51aab1d690d2c94f5451a2
syelman <syozleyen@gmail.com>
2023-02-14 19:17:31 +0100

    added src code

---
+++ src/gpu-spatial-graph-pipeline/anndata2data.py
@@ -0,0 +1,313 @@
+import torch
+import pandas as pd
+import numpy as np
+from torch_geometric.data import Data
+from scipy import sparse
+from abc import ABC, abstractmethod
+from .utils.transforms import get_from_address, get_adjacency_from_adata
+from typing import Optional, List, Callable, Any, Dict, Union
+
+
+class AnnData2Data(ABC):
+    def __init__(
+        self,
+        fields: Dict[str, List[str]],
+        adata_iter: Optional[Callable[[Any], Any]] = None,
+        preprocess: Optional[List[Callable[[Any, Dict[str, List[str]]], None]]] = None,
+        *args: Any,
+        **kwargs: Any
+    ) -> None:
+        """
+        Initializes an AnnData to PyTorch Data object converter.
+
+        Args:
+            fields: A dictionary that maps field names to a list of addresses.
+                Each address specifies the path to a numpy array in the AnnData object
+                (i.e., `fields[field_name] = ['attribute/key', 'attribute/key', ...]`).
+                (e.g.,  'features':['obs/Cluster_preprocessed','obs/donor','obsm/design_matrix'],
+                        'labels':['X']').
+            adata_iter: A function that returns an iterable of AnnData objects. 
+                This function will be used to extract multiple sub-AnnData objects from a larger AnnData object.
+                If set to None, the object will assume adata_iter returns only the input AnnData object.
+            preprocess: A list of callables that take an AnnData object and the fields dictionary as inputs and 
+                perform data preprocessing steps on the AnnData object before conversion to PyTorch Data objects.
+            *args: Any additional arguments to be passed to subclass constructors.
+            **kwargs: Any additional keyword arguments to be passed to subclass constructors.
+        """
+        self._adata_iter = adata_iter
+        self._preprocess = preprocess
+        self.fields = fields
+
+    @abstractmethod
+    def get_adj_matrix(self, adata: Any, *args: Any, **kwargs: Any) -> Any:
+        """
+        Abstract method for computing adjacency matrix from AnnData object.
+
+        Args:
+            adata: AnnData object.
+
+        Returns:
+            Adjacency matrix.
+        """
+        pass
+
+    def get_edge_index(self, adata: Any, adj_matrix: Any) -> Any:
+        """
+        Computes edge index tensor from adjacency matrix.
+
+        Args:
+            adata: AnnData object.
+            adj_matrix: Adjacency matrix.
+
+        Returns:
+            Edge index tensor.
+        """
+        nodes1, nodes2 = adj_matrix.nonzero()
+        return torch.vstack(
+            [
+                torch.from_numpy(nodes1).to(torch.long),
+                torch.from_numpy(nodes2).to(torch.long),
+            ]
+        )
+
+    @abstractmethod
+    def array_from_address(self, adata: Any, address: str, *args: Any, **kwargs: Any) -> np.ndarray:
+        """
+        Abstract method for retrieving a numpy array from an AnnData object.
+
+        Args:
+            adata: AnnData object.
+            address: Tuple of key and attribute for the numpy array.
+
+        Returns:
+            Numpy array.
+        """
+        pass
+
+    def create_data_obj(self, adata: Any, adj_matrix: Any) -> Data:
+        """
+        Creates a PyTorch Data object from an AnnData object.
+
+        Args:
+            adata: AnnData object.
+            adj_matrix: Adjacency matrix.
+
+        Returns:
+            PyTorch Data object.
+        """
+        obj = dict()
+        if self.yields_edge_index:
+            obj["edge_index"] = self.get_edge_index(adata, adj_matrix)
+
+        for field, addresses in self.fields.items():
+            arrs = []
+            for address in addresses:
+                arrs.append(self.array_from_address(adata, address))
+            obj[field] = torch.from_numpy(np.concatenate(arrs, axis=-1)).to(torch.float)
+
+        return Data(**obj)
+
+    def __call__(self, adata: Any) -> List[Any]:
+        """
+        Convert an AnnData object to a list of PyTorch compatible data objects.
+
+        Args:
+            adata: The AnnData object to be converted.
+
+        Returns:
+            A list of PyTorch compatible data objects.
+        """
+        dataset = []
+        # do the given preprocessing steps.
+        for process in self._preprocess:
+            process(adata, self.fields)
+        # iterate trough adata.
+        for subadata in self._adata_iter(adata):
+            adj_matrix = self.get_adj_matrix(subadata)
+            data = self.create_data_obj(subadata, adj_matrix)
+            dataset.append(data)
+
+        return dataset
+
+
+class AnnData2DataDefault(AnnData2Data):
+    def __init__(
+        self, 
+        fields: Dict[str, List[str]], 
+        adata_iter: Union[None, Callable[[Any], Any]] = None,
+        preprocess: Union[None, List[Callable[[Any], Any]]] = None,
+        yields_edge_index: bool = True
+    ) -> None:
+        """
+        Convert anndata object into a dictionary of arrays.
+
+        Assumes adata.obsp["adjacency_matrix_connectivities"] exists
+        if not it is computed with sq.gr.spatial_neighbors.
+        Works for squidpy datasets and the datasets of this package.
+
+        Example for fields argument:
+            fields = {
+                'features': ['obs/Cluster', 'obs/donor'],
+                'labels': ['X']
+            }
+
+        Args:
+            fields: A dictionary of field names and their addresses in the AnnData object.
+            adata_iter: An iterator function that returns an AnnData object.
+            preprocess: A list of functions to preprocess the input data.
+                This class by default adds a preprocessing step.
+                See the static method default_preprocess.
+            yields_edge_index: Whether to return the edge index of the adjacency matrix.
+        """
+        super().__init__(fields, adata_iter, preprocess)
+        # Default is the identity function.
+        self._adata_iter = adata_iter
+        self._preprocess = [AnnData2DataDefault.convert_to_array]
+        # Add preprocessing of the addresses to last.
+        # So that get_as_array works properly.
+        self._preprocess = (
+            preprocess if preprocess is not None else []
+        ) + self._preprocess
+        self.yields_edge_index = yields_edge_index
+
+    def get_adj_matrix(
+        self, adata: Any, *args: Any, **kwargs: Any
+    ) -> np.ndarray:
+        """Helper function to create an adjacency matrix depending on the anndata object.
+
+        Args:
+            adata: An AnnData object.
+            args: Additional arguments passed to the function get_adjacency_from_adata.
+            kwargs: Additional keyword arguments passed to the function get_adjacency_from_adata.
+
+        Returns:
+            The adjacency matrix.
+        """
+        # Get adjacency matrices
+        return get_adjacency_from_adata(adata, *args, **kwargs)
+
+    def array_from_address(
+        self, adata: Any, address: str
+    ) -> Union[np.ndarray, sparse.spmatrix]:
+        """Return the array corresponding to the given address.
+
+        This version assumes the addresses are stored on adata.uns.
+
+        Args:
+            adata: An AnnData object.
+            address: The address of the field in the anndata object.
+
+        Returns:
+            A numpy array or a sparse matrix.
+        """
+        processed_address = adata.uns["processed_index"][address]
+        return get_from_address(adata, processed_address)
+
+    @staticmethod
+    def convert_to_array(adata: Any, fields: Dict[str, List[str]]) -> None:
+        """
+        Store processed data in `obsm` field of `adata`. 
+        Store the new address for each processed data in `uns` field of `adata`.
+
+        Parameters
+        ----------
+        adata : Any
+            AnnData object.
+        fields : Dict[str, List[str]]
+            Dictionary of fields and their associated attributes.
+
+        Returns
+        -------
+        None
+        """
+        adata.uns["processed_index"] = dict()
+
+        for _, addresses in fields.items():
+
+            for address in addresses:
+                last_attr = address.split("/")[-1]
+                save_name = last_attr + "_processed"
+                # TODO: Is adding a suffix a good idea?
+
+                obj = get_from_address(adata, address)
+
+                # if obj is categorical
+                if obj.dtype.name == "category":
+                    adata.obsm[save_name] = pd.get_dummies(obj).to_numpy()
+                    adata.uns["processed_index"][address] = "obsm/" + save_name
+                elif not np.issubdtype(obj.dtype, np.number):
+                    adata.obsm[save_name] = obj.astype(np.float)
+                    adata.uns["processed_index"][address] = "obsm/" + save_name
+                elif sparse.issparse(obj):
+                    adata.obsm[save_name] = np.array(obj.todense())
+                    adata.uns["processed_index"][address] = "obsm/" + save_name
+
+                # If no storing required
+                else:
+                    adata.uns["processed_index"][address] = address
+
+
+class AnnData2DataByCategory(AnnData2DataDefault):
+    """
+    A class to transform AnnData objects into Data objects by category.
+
+    Parameters
+    ----------
+    fields : dict
+        Dictionary of fields and their associated attributes.
+    category : str
+        Column in `obs` containing the categories.
+    preprocess : callable, optional
+        Function to preprocess data before transformation.
+    yields_edge_index : bool, default=True
+        Whether to yield edge index.
+
+    Attributes
+    ----------
+    adata_iter : callable
+        Function to iterate over `adata` by category.
+    """
+    def __init__(self, fields: dict, category: str, preprocess=None, yields_edge_index: bool=True):
+        """
+        Initializes the class.
+
+        Parameters
+        ----------
+        fields : dict
+            Dictionary of fields and their associated attributes.
+        category : str
+            Column in `obs` containing the categories.
+        preprocess : callable, optional
+            Function to preprocess data before transformation.
+        yields_edge_index : bool, default=True
+            Whether to yield edge index.
+        """
+        super().__init__(
+            fields=fields,
+            adata_iter=lambda x: AnnData2DataByCategory.adata_iter_category(
+                x, category
+            ),
+            preprocess=preprocess,
+            yields_edge_index=yields_edge_index,
+        )
+
+    @staticmethod
+    def adata_iter_category(adata: Any, c: str) -> Any:
+        """
+        Iterates over `adata` by category.
+
+        Parameters
+        ----------
+        adata : Any
+            AnnData object.
+        c : str
+            Column in `obs` containing the categories.
+
+        Yields
+        -------
+        Any
+            Data object by category.
+        """
+        cats = adata.obs[c].dtypes.categories
+        for cat in cats:
+            yield adata[adata.obs[c] == cat]
---
+++ src/gpu-spatial-graph-pipeline/datamodule.py
@@ -0,0 +1,184 @@
+import pytorch_lightning as pl
+from typing import Callable, Literal, Optional, Sequence, Union, List
+from torch_geometric.loader import NeighborLoader, DataListLoader
+from torch_geometric.data import Data, Batch
+from torch_geometric.transforms import RandomNodeSplit
+from anndata import AnnData
+
+VALID_STAGE = {"fit", "test", None}
+VALID_SPLIT = {"node", "graph"}
+
+# TODO: Fix dataloader
+
+
+class GraphAnnDataModule(pl.LightningDataModule):
+    def __init__(
+        self,
+        datas: Optional[Sequence[Data]] = None,
+        batch_size: int = 1,
+        num_workers: int = 1,
+        learning_type: Literal["node", "graph"] = "node",
+    ):
+        """
+        Manages loading and sampling schemes before loading to GPU.
+
+        Args:
+            datas (Sequence[Data], optional): The data to be loaded. Defaults to None.
+            batch_size (int, optional): The batch size. Defaults to 1.
+            num_workers (int, optional): The number of workers. Defaults to 1.
+            learning_type (Literal["node", "graph"], optional): The type of learning to be performed.
+                If "graph" is selected, `batch_size` means the number of graphs and `datas` is expected to be a list of Data.
+                If "node" is selected, `batch_size` means the number of nodes and `datas` is expected to be a list of Data objects
+                with an edge_index attribute. Defaults to "node".
+
+        Raises:
+            ValueError: If `learning_type` is not one of {"node", "graph"}.
+        """
+        # TODO: Fill the docstring
+
+        super().__init__()
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+        self.data = datas
+        if learning_type not in VALID_SPLIT:
+            raise ValueError("Learning type must be one of %r." % VALID_SPLIT)
+        self.learning_type = learning_type
+        self.first_time = True
+
+    def _nodewise_setup(self, stage: Optional[str]) -> None:
+        """
+        Sets up the data loaders for node-wise learning.
+
+        Args:
+            stage (Optional[str]): The stage of training to set up the data loader for. Defaults to None.
+
+        Returns:
+            None
+        """
+        if self.first_time:
+            self.data = Batch.from_data_list(self.data)
+            self.transform = RandomNodeSplit(
+                split="train_rest",
+                num_val=max(int(self.data.num_nodes * 0.1), 1),
+                num_test=max(int(self.data.num_nodes * 0.05), 1),
+            )
+
+            self.data = self.transform(self.data)
+            self.first_time = False
+
+        if stage == "fit" or stage is None:
+            self._train_dataloader = self._spatial_node_loader(
+                input_nodes=self.data.train_mask, shuffle=True
+            )
+            self._val_dataloader = self._spatial_node_loader(
+                input_nodes=self.data.val_mask,
+            )
+        if stage == "test" or stage is None:
+            self._test_dataloader = self._spatial_node_loader(
+                input_nodes=self.data.test_mask,
+            )
+
+    def _graphwise_setup(self, stage: Optional[str]) -> None:
+        """
+        Sets up the data loaders for graph-wise learning.
+
+        Args:
+            stage (Optional[str]): The stage of training to set up the data loader for. Defaults to None.
+
+        Returns:
+            None
+        """
+        num_val = int(len(self.data) * 0.05 + 1)
+        num_test = int(len(self.data) * 0.01 + 1)
+
+        if stage == "fit" or stage is None:
+            self._train_dataloader = self._graph_loader(
+                data=self.data[num_val + num_test :],
+                shuffle=True,
+            )
+            self._val_dataloader = self._graph_loader(data=self.data[:num_val])
+        if stage == "test" or stage is None:
+            self._test_dataloader = self._graph_loader(
+                data=self.data[num_val : num_val + num_test]
+            )
+
+    def setup(self, stage: Optional[str] = None):
+        """
+        Setup function to be called at the beginning of training, validation or testing
+
+        Args:
+            stage (str, optional): the stage of the training, either 'train', 'val' or 'test'. Defaults to None.
+        """
+        # TODO: Implement each case
+        # TODO: Splitting
+        # stage = "train" if not stage else stage
+
+
+        if stage not in VALID_STAGE:
+            raise ValueError("Stage must be one of %r." % VALID_STAGE)
+
+        if self.learning_type == "graph":
+            if len(self.data) <= 3:
+                raise RuntimeError("Not enough graphs in data to do graph-wise learning")
+            self._graphwise_setup(stage)
+
+        else:
+            self._nodewise_setup(stage)
+
+    def train_dataloader(self):
+        """
+        Returns the training dataloader
+        """
+        return self._train_dataloader
+
+    def val_dataloader(self):
+        """
+        Returns the validation dataloader
+        """
+        return self._val_dataloader
+
+    def test_dataloader(self):
+        """
+        Returns the test dataloader
+        """
+        return self._test_dataloader
+
+    def _graph_loader(self, data: List, shuffle: bool = False, **kwargs) -> DataListLoader:
+        """
+        Loads the data in the form of graphs
+
+        Args:
+            data (List): list of data to be loaded
+            shuffle (bool, optional): whether to shuffle the data. Defaults to False.
+
+        Returns:
+            DataListLoader: the graph dataloader
+        """
+        return DataListLoader(
+            dataset=data,
+            shuffle=shuffle,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            **kwargs
+        )
+
+    def _spatial_node_loader(self, input_nodes: List, shuffle: bool = False, **kwargs) -> NeighborLoader:
+        """
+        Loads the data in the form of nodes
+
+        Args:
+            input_nodes (List): the input nodes
+            shuffle (bool, optional): whether to shuffle the data. Defaults to False.
+
+        Returns:
+            NeighborLoader: the node dataloader
+        """
+        return NeighborLoader(
+            self.data,
+            num_neighbors=[-1],
+            batch_size=self.batch_size,
+            input_nodes=input_nodes,
+            shuffle=shuffle,
+            num_workers=self.num_workers,
+            **kwargs
+        )
