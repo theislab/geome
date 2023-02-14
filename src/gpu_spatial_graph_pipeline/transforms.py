b67032708c77cbb88275104e10d8a761e6bd5bd2
Marcella Toth <toth.marcella@gmail.com>
2023-02-14 17:55:00 +0100

    docs and annot transforms

--- src/gpu_spatial_graph_pipeline/transforms.py
+++ src/gpu_spatial_graph_pipeline/transforms.py
@@ -1,9 +1,18 @@
 import numpy as np
 import squidpy as sq
 import pandas as pd
+from typing import Any
 
 
-def get_adjacency_from_adata(adata, *args, **kwargs):
+def get_adjacency_from_adata(adata: Any, *args: Any, **kwargs: Any) -> np.ndarray:
+    """Returns the spatial connectivities matrix from an AnnData object.
+
+    Args:
+        adata: The AnnData object.
+
+    Returns:
+        np.ndarray: The spatial connectivities matrix.
+    """
     if "adjacency_matrix_connectivities" in adata.obsp.keys():
         spatial_connectivities = adata.obsp["adjacency_matrix_connectivities"]
     else:
@@ -16,7 +25,17 @@ def get_adjacency_from_adata(adata, *args, **kwargs):
     return spatial_connectivities
 
 
-def design_matrix(A, Xl, Xc):
+def design_matrix(A: np.ndarray, Xl: np.ndarray, Xc: np.ndarray) -> np.ndarray:
+    """Returns the design matrix given the adjacency matrix, cell types, and domains.
+
+    Args:
+        A (np.ndarray): The adjacency matrix.
+        Xl (np.ndarray): The cell types.
+        Xc (np.ndarray): The domains.
+
+    Returns:
+        np.ndarray: The design matrix.
+    """
     N, L = Xl.shape
     Xs = (A @ Xl > 0).astype(np.float64)  # N x L
     Xts = np.einsum("bp,br->bpr", Xs, Xl).reshape((N, L * L))
@@ -25,18 +44,14 @@ def design_matrix(A, Xl, Xc):
     return Xd
 
 
-def add_design_matrix(adata, xl_name, xc_name, output_name):
-    """Adds design matrix to the given adata object to the given field.
-
-    A: Adj. matrix
-    Xl: Cell types
-    Xc: Domain
+def add_design_matrix (adata: Any, xl_name: str, xc_name: str, output_name: str) -> None:
+    """Adds the design matrix to the given AnnData object in the specified field.
 
     Args:
-        adata (_type_): _description_
-        xl_names (_type_): field name Xl is in adata.
-        xc_name (_type_): field name Xc is in adata.
-        output_name (_type_): Where to store the matrix in adata.
+        adata: The AnnData object.
+        xl_name (str): The name of the field containing the cell types.
+        xc_name (str): The name of the field containing the domains.
+        output_name (str): The name of the field to store the design matrix in.
     """
     adata.obsm[output_name] = design_matrix(
         get_adjacency_from_adata(adata),
@@ -45,7 +60,16 @@ def add_design_matrix(adata, xl_name, xc_name, output_name):
     )
 
 
-def get_from_address(adata, address):
+def get_from_address(adata: Any, address: str) -> Any:
+    """Gets the object at the given address from the AnnData object.
+
+    Args:
+        adata: The AnnData object.
+        address (str): The address of the object.
+
+    Returns:
+        Any: The object at the given address.
+    """
     # TODO check if address exists
     # TODO change function location
     attrs = address.split("/")
@@ -60,6 +84,12 @@ def get_from_address(adata, address):
     return obj
 
 
-def categorize_obs(adata, obs_list):
+def categorize_obs(adata: Any, obs_list: list[str]) -> None:
+    """Converts the given list of observation columns in the AnnData object to categorical.
+
+    Args:
+        adata: The AnnData object.
+        obs_list (list[str]): The list of observation columns to convert to categorical.
+    """
     for cat in obs_list:
         adata.obs[cat] = adata.obs[cat].astype('category')
