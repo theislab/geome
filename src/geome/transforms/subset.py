30d0a8467420e3a2c595ecd73f3cd5372aa07c54
selmanozleyen <syozleyen@gmail.com>
2024-05-16 23:00:51 +0200

    refactor and add tests

--- src/geome/transforms/subset.py
+++ src/geome/transforms/subset.py
@@ -1,58 +1,67 @@
+from __future__ import annotations
+
 from typing import Literal
 
+import numpy as np
 from anndata import AnnData
 
 from .base.transform import Transform
 
 
 class Subset(Transform):
-    """Create a subset of adata on list of observation or features.
+    """
+    Create a subset of an AnnData object based on specified observation or feature values.
 
-    Input:
-        key_value   : dict{str(obs_name): list[str(Value1), str(Value2),...],...}
-                        The dictionary with observation columns and list of values to keep for each observation.
+    Parameters
+    ----------
+    key_value : dict
+        A dictionary where keys are observation or feature names, and values are lists of values to keep.
+    axis : Literal[0, 1, "obs", "var"], optional
+        The axis to subset on. It can be 0 or "obs" for observations and 1 or "var" for features.
+        Default is "obs".
+    copy : bool, optional
+        If True, return a copy of the subsetted AnnData object instead of a view. Default is False.
+
+    Example:
+        import anndata as ad
+        import numpy as np
+        from geome.transforms import Subset
+
+        # Create test data
+        obs_data = {"cell_type": ["B cell", "T cell", "B cell", "T cell"]}
+        var_data = {"gene": ["gene1", "gene2", "gene3", "gene4"]}
+        adata = ad.AnnData(X=np.random.rand(4, 4), obs=obs_data, var=var_data)
+
+        # Subset by observations
+        adata_subset_obs = Subset(key_value={"cell_type": ["B cell"]}, axis="obs")(adata)
+        print(adata_subset_obs)
+        # View of AnnData object with n_obs x n_vars = 2 x 4
+        #    obs: 'cell_type'
+        #    var: 'gene'
     """
 
-    def __init__(self, key_value: dict, axis: Literal[0, 1, "obs", "var"] = "obs"):
+    def __init__(self, key_value: dict, axis: Literal[0, 1, "obs", "var"] = "obs", copy: bool = False):
         self.key_value = key_value
         if axis not in (0, 1, "obs", "var"):
             raise TypeError("axis needs to be one of obs, var, 0 or 1")
         if isinstance(axis, int):
             axis = ("obs", "var")[axis]
         self.axis = axis
+        self.copy = copy
 
     def __call__(self, adata: AnnData):
-        """Converts the given list of observation columns in the AnnData object to categorical.
-
-        Args:
-        ----
-        adata: The AnnData object.
-        obs_list (list[str]): The list of observation columns to convert to categorical.
-        """
+        """Subset the AnnData object based on the provided key_value and axis."""
+        subset_mask = self._generate_subset_mask(adata, self.axis)
         if self.axis == "obs":
-            subset_mask = self._generate_obs_subset_mask(adata)
             sub_adata = adata[subset_mask]
         elif self.axis == "var":
-            subset_mask = self._generate_var_subset_mask(adata)
             sub_adata = adata[:, subset_mask]
-        return sub_adata
+        return sub_adata.copy() if self.copy else sub_adata
 
-    def _generate_obs_subset_mask(self, adata: AnnData):
-        """Generate a boolean mask for selecting observations."""
-        subset_mask = None
+    def _generate_subset_mask(self, adata: AnnData, axis: Literal["obs", "var"]):
+        """Generate a boolean mask for selecting observations or variables based on the specified axis."""
+        data_attr = adata.obs if axis == "obs" else adata.var
+        subset_mask = np.ones(data_attr.shape[0], dtype=bool)
         for key, values in self.key_value.items():
-            if subset_mask is None:
-                subset_mask = adata.obs[key].isin(values)
-            else:
-                subset_mask &= adata.obs[key].isin(values)
-        return subset_mask
-
-    def _generate_var_subset_mask(self, adata: AnnData):
-        """Generate a boolean mask for selecting variables."""
-        subset_mask = None
-        for key, values in self.key_value.items():
-            if subset_mask is None:
-                subset_mask = adata.var[key].isin(values)
-            else:
-                subset_mask &= adata.var[key].isin(values)
+            subset_mask &= data_attr[key].isin(values)
         return subset_mask
